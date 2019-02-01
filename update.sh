e975f4ac9b424513be50240b6057b1ce0be26af2
Evaldo Klock <neto.nemesis@gmail.com>
2019-02-01 18:14:38 -0300

    Atualizações para funcionar no branch open-release/hawthorn.master

--- update.sh
+++ update.sh
@@ -1,15 +1,9 @@
-#!/bin/bash
-
 # update assets as edxapp user
 sudo -H -u edxapp bash << EOF
 source /edx/app/edxapp/edxapp_env
 cd /edx/app/edxapp/edx-platform
-paver update_assets lms --settings=production --debug --themes=edx.ufmt-online
-#paver update_assets cms --settings=aws --debug --themes=edx
+paver update_assets lms --settings=aws --themes=edx.ufmt-online
 EOF
-
-# restart lms instance
+# restart edx instances
 /edx/bin/supervisorctl restart lms
-#/edx/bin/supervisorctl restart cms
 /edx/bin/supervisorctl restart edxapp_worker:
-
