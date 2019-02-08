8bb0e42aa520d5ff7589811689789834d7cf6e0e
Evaldo Klock <neto.nemesis@gmail.com>
2019-02-08 11:11:08 -0300

    mais correções no tema

--- update.sh
+++ update.sh
@@ -2,8 +2,9 @@
 sudo -H -u edxapp bash << EOF
 source /edx/app/edxapp/edxapp_env
 cd /edx/app/edxapp/edx-platform
-paver update_assets lms --settings=aws --themes=edx.ufmt-online
+# paver update_assets lms --settings=aws --themes=edx.ufmt-online
+paver update_assets cms --settings=aws --themes=edx.ufmt-online
 EOF
 # restart edx instances
-/edx/bin/supervisorctl restart lms
+/edx/bin/supervisorctl restart cms
 /edx/bin/supervisorctl restart edxapp_worker:
